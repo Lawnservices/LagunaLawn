ff5856c1f369dd1b73dc0aee924d42798f210ec0
miguel <80590513+Lawnservices@users.noreply.github.com>
2022-11-17 16:52:51 -0500

    fix

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-// function myFunction() {
-//     var x = document.getElementById("myTopnav");
-//     if (x.className === "topnav") {
-//         x.className += " responsive";
-//     } else {
-//         x.className = "topnav";
-//     }
-// }
+ function myFunction() {
+     var x = document.getElementById("myTopnav");
+     if (x.className === "topnav") {
+         x.className += " responsive";
+     } else {
+         x.className = "topnav";
+     }
+ }
 
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-        x.className += " responsive";
-    } else {
-        x.className = "topnav";
-    }
-}
+ function setup() {
+     var textInput;
+     textInput = document.getElementById('username');
+     textInput.focus();
+     textInput = document.getElementById('enviar');
+ }
+ window.addEventListener('load', setup, false);
