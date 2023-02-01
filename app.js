f3350013b49b626ceacaf2c816a3b264e05080f4
Miguel Laguna <80590513+Lawnservices@users.noreply.github.com>
2023-02-01 14:43:38 -0500

    Update app.js

--- app.js
+++ app.js
@@ -11,6 +11,7 @@
      document.getElementById("col").style.backgroundColor = "#0366";
      document.getElementById("boton1").style.display = "none";
      document.getElementById("boton2").style.display = "block";
+   document.getElementById("myTopnav").style.backgroundColor = "#0366b6";
      document.getElementById("mnbv").innerHTML = "Thanks for visiting us.";
  }
 
