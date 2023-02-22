58749adafa4dd592126ab502bdfeedeaf7a23435
miguel <80590513+Lawnservices@users.noreply.github.com>
2023-02-22 13:10:02 -0500

    fix

--- app.js
+++ app.js
@@ -7,16 +7,4 @@
      }
  }
 
- function laguna() {
-     document.getElementById("col").style.backgroundColor = "#0366";
-     document.getElementById("boton1").style.display = "none";
-     document.getElementById("boton2").style.display = "block";
-   document.getElementById("myTopnav").style.backgroundColor = "#0366b6";
-     document.getElementById("mnbv").innerHTML = "Thanks for visiting us.";
- }
-
- function lagunas() {
-     document.getElementById("col").style.backgroundColor = "#fff";
-     document.getElementById("boton1").style.display = "block";
-     document.getElementById("boton2").style.display = "none";
- }
+  
